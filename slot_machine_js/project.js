8415bfb7e94046f37febe0cabfc885ded4f5f5ac
Yeasin Hossain <ysnhossain.107@gmail.com>
2024-02-10 11:27:40 +0600

    initialized the slot machine js project

---
+++ slot_machine_js/project.js
@@ -0,0 +1,15 @@
+// Create a folder for the project.
+// Initialize Node in the project.
+// Run on terminal: npm init
+// Add packages to take user inputs.
+// Run on terminal: npm i prompt-sync
+// Create a .js file and start coding.
+
+// Game rules:
+// 1. Deposit some money.
+// 2. Determine number of lines to bet on.
+// 3. Collect a bet amount.
+// 4. Spin the slot machine.
+// 5. Check if the user won.
+// 6. Give the user their winnings.
+// 7. Play again.
